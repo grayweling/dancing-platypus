ec251623e782a44b49d2d2ce9d48de03b46d5a68
dslpz <dslpz728@gmail.com>
2022-04-05 16:28:08 -0500

    added dashboard js to image

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -12,9 +12,17 @@ function Menu(e) {
       list.classList.remove(opacity - 100));
 }
 
-var button = document.getElementById("clickme"),
+var button = document.getElementById("platypus"),
   count = 0;
 button.onclick = function () {
   count += 1;
   button.innerHTML = "Click me: " + count;
 };
+
+
+/* 
+on first click of an image, counter with 60 seconds start. show timer decreasing.
+once they click the image, count clicks. show number increasing.
+once timer === zero, reset timer, stop counting, hide image, show the result with count with [Play Again] button.
+once click [Play Again], reset game and show image
+*/
