f03a31c5f17a68ac6f5155780c6a5076c6ffb210
grayweling <jy6228@yahoo.com>
2022-04-09 20:29:31 -0500

    troubleshooting heroku

--- server.js
+++ server.js
@@ -34,9 +34,9 @@ app.use(express.static(path.join(__dirname, 'public')));
   
 app.use(require('./controllers/'));
 
-app.get('/', (req, res) => {
-  res.render('main')
-})
+// app.get('/', (req, res) => {
+//   res.render('main')
+// })
   
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
